7287ac48962474826032000260f8fa3f7dcff454
Hồ Quang Trí <hoquangtri289@hotmail.com>
2020-07-27 15:07:00 +0700

    Edit

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,51 +1,64 @@
-import React, {useReducer, useEffect} from 'react';
+import React, { useReducer, useEffect, useState } from "react";
 
 let initLoading = {
     loading: true,
     data: [],
-    error: ''
-}
+    error: "",
+};
 
 let reducer = (state, action) => {
-    switch(action.type){
-        case 'FETCH_LOADING': {
+    switch (action.type) {
+        case "FETCH_LOADING": {
             return {
                 loading: false,
                 data: action.data,
-                error: ''
-            }
+                error: "",
+            };
         }
-        case 'FETCH_ERROR': {
-            return{
+        case "FETCH_ERROR": {
+            return {
                 loading: false,
                 data: [],
-                error: 'Error: fail to fetch'
-            }
+                error: "Error: fail to fetch",
+            };
         }
 
         default: {
             return state;
         }
     }
-}
+};
 const Customers = () => {
- const [customers, dispatch] = useReducer(reducer, initLoading);
- useEffect(() => {
-     fetch('http://localhost:3000/api/customers')
-     .then(response => response.json())
-     .then(data => {
-         console.log(data); // co roiroi
-         dispatch({type: 'FETCH_LOADING', data});
-     })
-     .catch(error => {
-         dispatch({type: 'FETCH_ERROR'});
-     })
- }, [])
- return(
-     <div>
-         Cho du lieu ra day
-     </div>
- )
-}
+    const [customers, dispatch] = useReducer(reducer, initLoading);
+    const [page, setPage] = useState({
+        prePage: 0,
+        nextPage: 10
+    })
+    useEffect(() => {
+        fetch("http://localhost:3000/api/customers")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); // co roiroi
+                dispatch({ type: "FETCH_LOADING", data: data });
+            })
+            .catch((error) => {
+                dispatch({ type: "FETCH_ERROR" });
+            });
+    }, []);
+
+    return (
+        <React.Fragment>
+            <ul>
+                {customers.loading
+                    ? "Loading..."
+                    : customers.data.map((doc) => {
+                          return <li>{doc.id}</li>;
+                      })}
+
+                {customers.error ? customers.error : null}
+            </ul>
+        </React.Fragment>
+    );
+};
 
 export default Customers;
